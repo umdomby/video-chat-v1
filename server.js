65872b142d62539cf2ad990d82525c1126ac4a91
S.Kuntsevich <umdomby@yandex.ru>
2022-01-25 08:31:19 +0300

    first commit

--- server.js
+++ server.js
@@ -1,28 +1,51 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
+const express = require('express')
+const app = express()
+const servac = require('https')
+const fs = require('fs');
+const path = require('path')
+const { v4: uuidV4 } = require('uuid')
 app.set("view engine", "ejs");
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
+
+app.use(express.static('public'))
+
+const privateKey = fs.readFileSync(path.resolve(__dirname,'./cert/privkey.pem'));
+const certificate = fs.readFileSync(path.resolve(__dirname,'./cert/cert.pem'));
+const ca = fs.readFileSync(path.resolve(__dirname,'./cert/chain.pem'));
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+const server = servac.createServer(credentials, app);
+
+
+
+
+
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
-});
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
-});
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
@@ -34,4 +57,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(443);
